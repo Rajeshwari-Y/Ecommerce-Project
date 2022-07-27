2187f42335c813fdac17d1608ee967f355ab76a2
unknown <rajeshwari.yelagandula@gmail.com>
2022-07-27 21:54:16 +0200

    initial-commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/raji/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.raji.ecommerce.dao;
+
+import com.raji.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
